ab428f1a7455f5aef2e7f9f0700decc0183c6710
Rounak <rounakadhikari1@gmail.com>
2026-03-25 19:43:24 +0545

    to make response and for dash to work

--- src/pages/StyleGuide.tsx
+++ src/pages/StyleGuide.tsx
@@ -2,7 +2,7 @@ const StyleGuide = () => {
   return (
     <div style={{ padding: "2rem", color: "var(--text-primary)" }}>
       <h1>Style Guide</h1>
-      <p>This page has been intentionally cleared. Coming soon.</p>
+      <p>deleted.</p>
     </div>
   );
 };
